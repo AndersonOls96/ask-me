39c7c7ae0585b69706571bcffd5129bb7f20d491
AndersonOls96 <11052@senacminas.edu.br>
2024-07-01 11:40:38 -0300

    Configuração do banco de dados e criação do model de Pergunta

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 const express = require('express')
 const app = express()
 const bodyparser = require('body-parser')
+const connection = require('./database/database')
+const Pergunta = require('./database/Pergunta')
 const port = 3000
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Banco de dados conectado com sucesso!")
+    }).catch((msgErro) => {
+        console.log(msgErro)
+    })
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/perguntar', (req, res) =>{
+app.get('/perguntar', (req, res) => {
     res.render('perguntar')
 })
 
-app.post('/salvarpergunta', (req, res) =>{
+app.post('/salvarpergunta', (req, res) => {
     let titulo = req.body.titulo
     let descricao = req.body.descricao
     res.send(`Titulo: ${titulo} / Descrição: ${descricao}`)
 })
 
-app.listen(port, (erro) =>{
-    if(erro){
+app.listen(port, (erro) => {
+    if (erro) {
         console.log("Erro ao iniciar o servidor")
-    }else{
+    } else {
         console.log(`Servidor rodando em http://localhost:${port}`)
     }
 })
